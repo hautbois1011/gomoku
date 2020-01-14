1ad32c55a2cfccc0e8a2531aa9019490dd0ad8c4
hautbois1011 <subterranean_mirror@yahoo.co.jp>
2020-01-14 17:10:32 +0900

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,53 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+        cerr << "Initialization error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    atexit(SDL_Quit);
+
+    SDL_Window* window = SDL_CreateWindow(
+            "Gomoku",
+            SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED,
+            640,
+            480,
+            SDL_WINDOW_SHOWN
+            );
+
+    if(!window) {
+        cerr << "Window creation error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if(!renderer) {
+        cerr << "Renderer creation error: " << SDL_GetError() << endl;
+        return 1;
+    }
+
+    bool quit = false;
+    while(!quit) {
+        SDL_Event e;
+        while(SDL_PollEvent(&e)) {
+            if(e.type == SDL_QUIT) {
+                quit = true;
+                break;
+            }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    return 0;
+}
